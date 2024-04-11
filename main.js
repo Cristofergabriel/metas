2e8cd654bf998bf33c1038e5b013df39c3445141
Cristofergabriel <105427788+Cristofergabriel@users.noreply.github.com>
2024-04-11 08:46:02 -0300

    vsegre

--- main.js
+++ main.js
@@ -12,3 +12,5 @@ for(let i=0; i <botoes.length; i++){
     textos[i].classList.add("ativo");
 }
 }
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05t00:00:00");
